84ad8c84b928cda3e2c057d677683185ff3c577c
Marco Cordoni <marco.cordoni@studenti.unimi.it>
2018-11-18 18:24:02 +0100

    UFO Game

---
+++ Assets/Rotator.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotator : MonoBehaviour {
+
+	void Update () { // usiamo Update e non FixUpdate perchè non faccio uso di forze
+        transform.Rotate(new Vector3(0, 0, 45) * Time.deltaTime); // ruota attorno all'asse Z
+
+	}
+}
